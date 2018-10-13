4bb7d5e784468819b19456a9be689b586d677371
Thomas Kamm <thomaskamm@Thomas-MacBook-Pro.local>
2018-10-13 14:37:34 -0700

    report

--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -18,9 +18,33 @@ export default class SettingsScreen extends Component {
     title: 'Create Report',
   };
 
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    let {coords} = location;
+    let {latitude, longitude} = coords;
+    this.setState({latitude, longitude});
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <TextInput placeholder="Title"/>
         <TextInput value={this.state.description} placeholder="Description"/>
       </View>
     );

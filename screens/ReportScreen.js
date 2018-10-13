9d9b04dce4f6fa0739e5defbe63d9b0e35d5b81a
Aaron Janse <gitduino@gmail.com>
2018-10-13 14:37:37 -0700

    remove unused import

--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, TextInput, Platform, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 
 export default class SettingsScreen extends Component {
   constructor(props){

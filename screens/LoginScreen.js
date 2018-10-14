a84b35c72242ad09d3459174914e7c0ce43e7103
Arnav Verma <ArnavVerma@Arnavs-MacBook.local>
2018-10-14 01:42:57 -0700

    ha

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Alert, StyleSheet,Text,TouchableOpacity, View,TextInput, AsyncStorage} from 'react-native';
+import { ImageBackground } from 'react-native';
+import { Alert, StyleSheet,Text,TouchableOpacity, View,TextInput, Image, AsyncStorage} from 'react-native';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
@@ -50,36 +51,43 @@ export default class LoginScreen extends React.Component {
     this.props.navigation.navigate('Map')
   }
 
+
   render() {
     return (
-        <View style={styles.container}>
+      <ImageBackground style={ styles.container }
+                 resizeMode='cover'
+                 source={{uri: 'https://images.unsplash.com/photo-1529567186287-3e17bdefa342?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6bae897db75ab4c28184b94b73ccfec0&w=1000&q=80'}}>
+        <View style={styles.text}>
           <View style={styles.text}>
-            <Text style={{textAlign: 'center', fontSize: 35, marginBottom: 10}}> Welcome to EnviroReport</Text>
-            <Text style={{textAlign: 'center'}}> Please enter a few details about you to continue</Text>
+            <Text style={{textAlign: 'center', fontSize: 35, color: 'white', marginBottom: 10}}> Welcome to EnviroReport</Text>
+            <Text style={{textAlign: 'center', fontSize: 20, color: 'white', marginBottom: 0}}> Please enter a few details about you to continue</Text>
           </View>
-          <TextInput style={styles.input} value={this.state.name} onChangeText={(name) => this.setState({name})} placeholder="John Smith"/>
-          <TextInput style={styles.input} value={this.state.number} onChangeText={(number) => this.setState({number})} placeholder="phone number"/>
+          <TextInput style={styles.input} value={this.state.name} onChangeText={(name) => this.setState({name})} placeholder="First Last"/>
+          <TextInput style={styles.input} value={this.state.number} onChangeText={(number) => this.setState({number})} placeholder="Phone Number"/>
           <Button
             icon={
                 <Icon
                   name='arrow-right'
-                  size={15}
+                  size={40}
                   color='blue'
                   />
                 }
             buttonStyle={{
-                backgroundColor: "rgba(92, 99,216, 1)",
-                width: 300,
+                backgroundColor: "rgba(92,99,216,1)",
+                width: 200,
                 height: 45,
-                borderColor: "transparent",
+                borderColor: "white",
+                marginTop: 20,
                 borderWidth: 0,
-                borderRadius: 5
+                borderRadius: 5,
+                justifyContent: 'center',
+                alignItems: 'center'
               }}
-
                 title='GET STARTED'
                 onPress={this.handlePress}
                 />
         </View>
+        </ImageBackground>
     );
   }
 
@@ -91,23 +99,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+    opacity: 1000,
   },
   input: {
-    marginBottom: 15,
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    fontSize: 10,
+    marginBottom: 0,
+    borderBottomColor: 'rgba(255,255,255, 1)',
+    borderBottomWidth: 1.5,
+    fontSize: 16,
     height: 35,
-    width: 250,
-  },
-  button: {
-    height: 30,
-    width: 130,
-    marginTop: 30
+    width: 260,
   },
   text: {
     width: 300,
     marginBottom: 30,
+    alignItems: 'center',
     justifyContent: 'center'
   }
 });
